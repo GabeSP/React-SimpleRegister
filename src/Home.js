6006a72d5ee6f6433ad2c8d36f57c74ba2844519
Gabriel Pereira <ga_dsp@hotmail.com>
2018-07-05 17:47:21 -0300

    Class Home
    Classe que vai representar a Home da aplicação

---
+++ src/Home.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+
+  render() {
+
+    return (
+
+      <div>
+        <div className='header'>
+          <h1>Welcome</h1>
+        </div>
+        <div className='content' id='content'>
+          <h2>Library Register</h2> 
+        </div>
+      </div>
+    );
+  }
+}
